f96c08d988ac4dbb701851c111113157ebd7f253
turamant <tur1amant@gmail.com>
2021-05-28 11:36:42 +0300

    git init

---
+++ config/local_settings.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-jt%cxz-uww8-!sctkv)o=(5=avi*$i%pr%7q1)mxtvje@as55%'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
